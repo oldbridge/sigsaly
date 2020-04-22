681f0cedd7a28748362ef5a4397336b9df5c345a
Xabi Zubi <xabi.zubi@gmail.com>
2020-04-22 22:06:39 +0200

    Add filterbank
    * Add 150-2950 Hz filterbank for the channel vocoder
    * Used Chevyschev filters

--- vocoder.py
+++ vocoder.py
@@ -12,8 +12,26 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+from scipy.signal import sosfilt, cheby1
 
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
 
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def show_timeplot(frames, rate):
+    plt.plot(np.linspace(0, len(frames) / rate, len(frames)), frames)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude [normalized]")
+    plt.grid(True)
+        
 class AudioDevice():
     def __init__(self):
         self.format = pyaudio.paInt16
@@ -42,12 +60,7 @@ class AudioDevice():
     
     def __to_play_format(self, array):
         return (array * 32768).astype(np.int16).tostring()
-    
-    def show_timeplot(self):
-        plt.plot(np.linspace(0, len(self.frames) / self.rate, len(self.frames)), self.frames)
-        plt.xlabel("Time [s]")
-        plt.ylabel("Amplitude [normalized]")
-        plt.grid(True)
+        
     def play(self):
         stream = self.audio.open(format=self.format,
                                  rate=self.rate,
@@ -69,16 +82,67 @@ class AudioDevice():
         self.rate, data = wavfile.read(filename)
         # Play the sound by writing the audio data to the stream
         self.frames = data / 32768
+    
+    def bandpass_signal(self):
+        self.frames = butter_bandpass_filter(self.frames, 
+                                             150, 2950, self.rate)
+    
+class Vocoder():
+    def __init__(self, audio_device):
+        self.ad = audio_device
+        self.data = self.ad.frames
+        self.rate = self.ad.rate
+        self.filter_bank()
         
+    def plot_filtered(self):
+        fig, axs = plt.subplots(11, 1, sharex=True)
+        t = np.arange(0, len(self.data) / self.rate, 1/ self.rate)
+        axs[0].plot(t, self.data)
+        for i, signal in enumerate(self.filtered):
+            axs[i + 1].plot(t, signal)
+        axs[i + 1].set_xlabel('Time [s]')
+            
+    def filter_bank(self):
+        values = np.zeros((10, len(self.data)))
+        filter_bank = [[150, 350],
+                       [350, 550],
+                       [550, 750],
+                       [750, 1050],
+                       [1050, 1450],
+                       [1450, 1750],
+                       [1750, 2000],
+                       [2000, 2350],
+                       [2350, 2650],
+                       [2650, 2950]]
+        
+        # Low pass filter of 25 Hz
+        sos_lp = cheby1(10, 1, 25, 'lp', fs=self.rate, output='sos')
+        for i, band in enumerate(filter_bank):
+            print(f"Filtering from {band[0]} to {band[1]}")
+            # First apply bandpass
+            sos_bp = cheby1(10, 1, band, 'bandpass', fs=self.rate, output='sos')
+            filtered = sosfilt(sos_bp, self.data)
+            
+            # Rectify
+            filtered[filtered < 0] = 0
+            
+            # Apply 25 Hz lowpass filter
+            filtered = sosfilt(sos_lp, filtered)
+            values[i, :] = filtered
+        self.filtered = values
+                
 if __name__ == '__main__':
-    rec_time = 3
+    rec_time = 5
     a = AudioDevice()
     a.load_wav()
+    v = Vocoder(a)
+    v.plot_filtered()
     #a.record(rec_time)
-    a.show_timeplot()
+    #a.show_timeplot()
     #time.sleep(rec_time)
     #a.show_timeplot()
-    a.play()
+    #a.bandpass_signal()
+    #a.play()
     #a.save_wav()
     
     
